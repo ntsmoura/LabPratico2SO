58b809f7b57573bf56db3418c0fbb4455eca51d2
And Paiva <andre.pcr@hotmail.com>
2021-10-24 19:33:18 -0300

    Ordenação: algoritmo funcionando com threads

--- OrdenacaoMultithread/ordena_multithread.c
+++ OrdenacaoMultithread/ordena_multithread.c
@@ -8,71 +8,127 @@
 long long *vetor;
 unsigned int tamanho_vetor;
 
-/*
+void *thread_main(void*);
+void *thread_divide(void*);
+
 typedef struct{
-    int indice;
-    int tamanho;
+    unsigned int indice;
+    unsigned int tamanho;
 } args_divide;
 
 typedef struct{
-    int indice_a;
-    int tamanho_a;
-    int indice_b;
-    int tamanho_b;
+    unsigned int indice_a;
+    unsigned int tamanho_a;
+    unsigned int indice_b;
+    unsigned int tamanho_b;
 } args_main;
 
+int create_thread_main(unsigned int i_a, unsigned int t_a, unsigned int i_b, unsigned int t_b){
+    args_main *data = (args_main*) malloc(sizeof(args_main));
+    if(data == NULL){
+        return 1;
+    }
+    data->indice_a = i_a;
+    data->tamanho_a = t_a;
+    data->indice_b = i_b;
+    data->tamanho_b = t_b;
+
+    pthread_t *t_main = (pthread_t*)malloc(sizeof(pthread_t));
+    if(t_main == NULL){
+        free(data);
+        return 2;
+    }
+    int criacao_thread = pthread_create(t_main, NULL, thread_main, (void *) data);
+    if(criacao_thread != 0){
+        free(t_main);
+        free(data);
+        return 3;
+    }
+    pthread_join(*t_main, NULL);
+    free(t_main);
+    free(data);
+    return 0;
+}
+
+int create_thread_divide(unsigned int i, unsigned int t){
+    args_divide *data = (args_divide*) malloc(sizeof(args_divide));
+    if(data == NULL){
+        return 1;
+    }
+    data->indice = i;
+    data->tamanho = t;
+
+    pthread_t *t_divide = (pthread_t*)malloc(sizeof(pthread_t));
+    if(t_divide == NULL){
+        free(data);
+        return 2;
+    }
+    int criacao_thread = pthread_create(t_divide, NULL, thread_divide, (void *) data);
+    if(criacao_thread != 0){
+        free(t_divide);
+        free(data);
+        return 3;
+    }
+    pthread_join(*t_divide, NULL);
+    free(t_divide);
+    free(data);
+    return 0;
+}
+
 void *thread_main(void *args){
-
-}
-
-void *thread_divide(void *args){
-
-}
-*/
-
-void dividir(int i, unsigned int size);
-void ordenar(int a, unsigned int sizea, int b, unsigned int sizeb);
-
-void dividir(int i, unsigned int size){
-    int x, y;
-    unsigned int sizex, sizey;
-    x = i;
-    sizex = size - (size/2);
-    y = x + sizex;
-    sizey = size/2;
-    ordenar(x, sizex, y, sizey);
-}
-
-void ordenar(int a, unsigned int sizea, int b, unsigned int sizeb){
-    int p = a;
-    int q = b;
+    args_main *data = (args_main*) args;
+    int status;
+    int tmp_a = data->indice_a;
+    int tmp_b = data->indice_b;
     int cont = 0;
-    long long aux[sizea+sizeb];
-    if(sizea > 1){
-        dividir(a, sizea);
+    long long aux[(data->tamanho_a + data->tamanho_b)];
+    if(data->tamanho_a > 1){
+        status = create_thread_divide(data->indice_a, data->tamanho_a);
+        if(status != 0){
+            printf("ERRO.\n");
+            return 0;
+        }
     }
-    if(sizeb > 1){
-        dividir(b, sizeb);
+    if(data->tamanho_b > 1){
+        status = create_thread_divide(data->indice_b, data->tamanho_b);
+        if(status != 0){
+            printf("ERRO.\n");
+            return 0;
+        }
     }
-    while(cont < sizea+sizeb){
-        if(q >= b + sizeb){
-            aux[cont] = vetor[p];
-            p++;
-        }else if(p >= a + sizea){
-            aux[cont] = vetor[q];
-            q++;
-        }else if(vetor[p] <= vetor[q]){
-            aux[cont] = vetor[p];
-            p++;
-        }else if(vetor[p] > vetor[q]){
-            aux[cont] = vetor[q];
-            q++;
+    while(cont < (data->tamanho_a + data->tamanho_b)){
+        if(tmp_b >= data->indice_b + data->tamanho_b){
+            aux[cont] = vetor[tmp_a];
+            tmp_a++;
+        }else if(tmp_a >= data->indice_a + data->tamanho_a){
+            aux[cont] = vetor[tmp_b];
+            tmp_b++;
+        }else if(vetor[tmp_a] <= vetor[tmp_b]){
+            aux[cont] = vetor[tmp_a];
+            tmp_a++;
+        }else if(vetor[tmp_a] > vetor[tmp_b]){
+            aux[cont] = vetor[tmp_b];
+            tmp_b++;
         }
         cont++;
     }
-    for(cont = a; cont < a+sizea+sizeb; cont++){
-        vetor[cont] = aux[cont-a];
+    for(cont = 0; cont < (data->tamanho_a + data->tamanho_b); cont++){
+        vetor[data->indice_a + cont] = aux[cont];
     }
+    return 0;
+}
+
+void *thread_divide(void *args){
+    args_divide *data = (args_divide*) args;
+    unsigned int x, y;
+    unsigned int tam_x, tam_y;
+    x = data->indice;
+    tam_x = data->tamanho - ((data->tamanho)/2);
+    y = x + tam_x;
+    tam_y = (data->tamanho)/2;
+    int status = create_thread_main(x, tam_x, y, tam_y);
+    if(status != 0) printf("ERRO.\n");
+    return 0;
 }
 
 int main(int argc, char *argv[]){
@@ -82,9 +138,12 @@ int main(int argc, char *argv[]){
     for(count_input = 0; count_input < tamanho_vetor; count_input++)
         scanf("%lld", &vetor[count_input]);
 
-    ordenar(0, tamanho_vetor, 0, 0);
+    int status = create_thread_main(0, tamanho_vetor, 0, 0);
 
-    for(count_input = 0; count_input < tamanho_vetor; count_input++)
-        printf("%lld ", vetor[count_input]);    
+    if(status == 0){
+        for(count_input = 0; count_input < tamanho_vetor; count_input++)
+            printf("%lld ", vetor[count_input]);  
+        printf("\n"); 
+    }else printf("ERRO.\n");
     free(vetor);
 }
